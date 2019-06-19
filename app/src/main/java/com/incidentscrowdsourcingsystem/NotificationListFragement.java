af1c680339b827db23b6a057d797095006c69731
Ashenn1 <sohasanad@gmail.com>
2019-06-19 02:06:32 +0200

    notification history activity UI

---
+++ app/src/main/java/com/incidentscrowdsourcingsystem/NotificationListFragement.java
@@ -0,0 +1,13 @@
+package com.incidentscrowdsourcingsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationListFragement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_list_fragement);
+    }
+}
