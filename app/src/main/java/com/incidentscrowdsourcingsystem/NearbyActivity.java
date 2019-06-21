7344d58d52a4c05596af12f8987ff089577f59da
marwa <marwaothman97@yahoo.com>
2019-06-22 01:57:15 +0300

    updates

--- app/src/main/java/com/incidentscrowdsourcingsystem/NearbyActivity.java
+++ app/src/main/java/com/incidentscrowdsourcingsystem/NearbyActivity.java
@@ -1,7 +1,11 @@
 package com.incidentscrowdsourcingsystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class NearbyActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class NearbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby);
+
     }
+
 }
